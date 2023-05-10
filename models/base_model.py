8239760208f7dd9ede84adaa23c10c75cd6ac4d9
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-10 01:54:19 -0400

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,67 @@
-import uuid
 from datetime import datetime
+import models
+import uuid
 
 
 class BaseModel:
+    """Base Model Class
+
+    that defines all common attributes/methods for other classes
+    initialization, serialization and deserialization
+    of the future instances.
+    
     """
-    BaseModel class defines all common attributes/methods for other classes
-    """
+
     def __init__(self, *args, **kwargs):
         """
-        Initializes a new instance of the BaseModel class
+        Base Modele instance are initialized.
+
         """
         if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
+            for arg, val in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if arg != '__class__':
+                    setattr(self, arg, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
-        Returns a string representation of the BaseModel instance
+        Return [<class name>] (<self.id>) <self.__dict__>
+        Representation of the class for the user
+        
         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+            )
 
     def save(self):
-        """
-        Updates the public instance attribute updated_at with the current datetime
+        """Updates a Base Model instance
+
+        Updates the public instance attribute `updated_at`
+        with the current datetime and dumps the class data
+        into a file
+
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
+        """This method will be the first piece of the 
+        
+        serialization/deserialization process
+        create a dictionary representation with 
+        “simple object type” of our BaseModel
+        
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance
-        """
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = type(self).__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+
+        return new_dict
