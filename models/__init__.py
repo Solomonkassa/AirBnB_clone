25498ad6b199a2f57f20e79893db07d645a0b27d
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-10 01:57:41 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
+"""`Storage`  variable initializes  to create a
+unique `FileStorage` instance for the HBNB application.
 
+"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
