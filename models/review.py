6b245c3c1ce504559432f1a0cb665457c6d18773
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-11 16:38:55 -0400

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """The `review` module.
 
-It defines one class, `Review(),
 which sub-classes the `BaseModel()` class.`
+It defines one class, `Review(),
 """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
     """A review of a place/house.
-
-    It represents a review posted by the users
-    of the application about a place/house.
 
     Attributes:
         text
