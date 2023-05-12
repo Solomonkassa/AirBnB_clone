f6cfd5a6086c39579fdda26592ab756fb576061d
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-12 08:27:26 -0700

    Update console.py

--- console.py
+++ console.py
@@ -164,6 +164,14 @@ class HBNBCommand(cmd.Cmd):
             print("** attribute name missing **")
         except ValueError:
             print("** value missing **")
-            
+    def do_count(self, arg):
+        """ Counts number of instance of a class """
+        counter = 0
+        objects = storage.all()
+        for key in objects:
+            if (arg in key):
+                counter += 1
+        print(counter)
+           
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
