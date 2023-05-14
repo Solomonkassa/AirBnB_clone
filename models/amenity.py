6089a9e70ffdc4abe089e54452634713107edcf0
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-14 07:27:35 -0400

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""The `amenity` module
-
+"""
 It defines one class, `Amenity(),
 which sub-classes the `BaseModel()` class.`
 """
@@ -9,9 +8,6 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """An amenity provided by a place/house.
-
-    Attributes:
-        name
     """
 
     name = ""
