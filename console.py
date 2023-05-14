6029dc81111657100c02392badda02ce803e5282
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-14 13:16:57 -0700

    Update console.py

--- console.py
+++ console.py
@@ -29,39 +29,39 @@ class HBNBCommand(cmd.Cmd):
         """Defines instructions to execute before <line> is interpreted.
         """
         if not line:
-          return '\n'\
+            return '\n'\
     
         pattern = re.compile(r"(\w+)\.(\w+)\((.*)\)")
         match_list = pattern.findall(line)
         if not match_list:
-          return super().precmd(line)
+            return super().precmd(line)
 
         match_tuple = match_list[0]
         if not match_tuple[2]:
-          if match_tuple[1] == "count":
-            instance_objs = storage.all()
-            print(len([
-              v for _, v in instance_objs.items()
-              if type(v).__name__ == match_tuple[0]]))
-            return "\n"
-          return "{} {}".format(match_tuple[1], match_tuple[0])
+            if match_tuple[1] == "count":
+              instance_objs = storage.all()
+              print(len([
+                v for _, v in instance_objs.items()
+                if type(v).__name__ == match_tuple[0]]))
+              return "\n"
+            return "{} {}".format(match_tuple[1], match_tuple[0])
         else:
-          args = match_tuple[2].split(", ")
-          if len(args) == 1:
-            return "{} {} {}".format(
-              match_tuple[1], match_tuple[0],
-              re.sub("[\"\']", "", match_tuple[2]))
-          else:
-            match_json = re.findall(r"{.*}", match_tuple[2])
-            if (match_json):
-              return "{} {} {} {}".format(
-                match_tuple[1], match_tuple[0],
-                re.sub("[\"\']", "", args[0]),
-                re.sub("\'", "\"", match_json[0]))
-            return "{} {} {} {} {}".format(
-              match_tuple[1], match_tuple[0],
-              re.sub("[\"\']", "", args[0]),
-              re.sub("[\"\']", "", args[1]), args[2])
+            args = match_tuple[2].split(", ")
+            if len(args) == 1:
+                return "{} {} {}".format(
+                    match_tuple[1], match_tuple[0],
+                    re.sub("[\"\']", "", match_tuple[2]))
+            else:
+                match_json = re.findall(r"{.*}", match_tuple[2])
+                     if (match_json):
+                          return "{} {} {} {}".format(
+                    match_tuple[1], match_tuple[0],
+                    re.sub("[\"\']", "", args[0]),
+                    re.sub("\'", "\"", match_json[0]))
+                return "{} {} {} {} {}".format(
+                    match_tuple[1], match_tuple[0],
+                    re.sub("[\"\']", "", args[0]),
+                    re.sub("[\"\']", "", args[1]), args[2])
 
     def do_EOF(self, line):
         """Inbuilt EOF command to gracefully catch errors.
