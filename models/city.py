ab5686455efcccd5caa264e22528863f194c97b9
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-14 07:38:30 -0400

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""The `city` module
-
+"""
 It defines one class, `City(),
 which sub-classes the `BaseModel()` class.`
 """
@@ -8,11 +7,7 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """A city in the application.
-
-    Attributes:
-        name
-        state_id
+    """ The city class, contains state ID and name
     """
     name = ""
     state_id = ""
