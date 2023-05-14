b5eb4c0b103fdc79df2a778393ff9f950e14e4fe
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-14 04:29:48 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Module: __init__.py
-"""
+"""This module instantiates an object of class FileStorage"""
 from models.engine import file_storage
 
 storage = file_storage.FileStorage()
