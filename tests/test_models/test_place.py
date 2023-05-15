77f3e0b9a2d38ae4b2e76ff999e9b8655d03ce54
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-15 01:45:32 -0400

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,19 +10,22 @@ from datetime import datetime
 
 
 class TestPlace(unittest.TestCase):
-    """Test cases for the `Place` class."""
+    """Test cases for the `Place` class.
+    """
 
     def setUp(self):
         pass
 
     def tearDown(self) -> None:
-        """Resets FileStorage data."""
+        """Resets FileStorage data.
+        """
         FileStorage._FileStorage__objects = {}
         if os.path.exists(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_params(self):
-        """Test method for class attributes."""
+        """Test method for class attributes.
+        """
 
         p1 = Place()
         p3 = Place("hello", "wait", "in")
@@ -44,7 +47,8 @@ class TestPlace(unittest.TestCase):
         self.assertIsInstance(p1.amenity_ids, list)
 
     def test_init(self):
-        """Test method for public instances."""
+        """Test method for public instances.
+        """
 
         p1 = Place()
         p2 = Place(**p1.to_dict())
@@ -54,20 +58,23 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(p1.updated_at, p2.updated_at)
 
     def test_str(self):
-        """Test method for str representation."""
+        """Test method for str representation.
+        """
         p1 = Place()
         string = f"[{type(p1).__name__}] ({p1.id}) {p1.__dict__}"
         self.assertEqual(p1.__str__(), string)
 
     def test_save(self):
-        """Test method for save."""
+        """Test method for save.
+        """
         p1 = Place()
         old_update = p1.updated_at
         p1.save()
         self.assertNotEqual(p1.updated_at, old_update)
 
     def test_todict(self):
-        """Test method for dict."""
+        """Test method for dict.
+        """
         p1 = Place()
         p2 = Place(**p1.to_dict())
         a_dict = p2.to_dict()
