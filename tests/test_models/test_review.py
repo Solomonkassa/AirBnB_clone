e2e53f2eedbdd8c0194c2d6041462d5236ff57bf
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-15 01:46:29 -0400

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -10,19 +10,22 @@ from models.engine.file_storage import FileStorage
 
 
 class TestReview(unittest.TestCase):
-    """Test cases for the `Review` class."""
+    """Test cases for the `Review` class.
+    """
 
     def setUp(self):
         pass
 
     def tearDown(self) -> None:
-        """Resets FileStorage data."""
+        """Resets FileStorage data.
+        """
         FileStorage._FileStorage__objects = {}
         if os.path.exists(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_params(self):
-        """Test method for class attributes"""
+        """Test method for class attributes
+        """
 
         r1 = Review()
         r3 = Review("hello", "wait", "in")
@@ -33,7 +36,8 @@ class TestReview(unittest.TestCase):
         self.assertEqual(r3.text, "")
 
     def test_init(self):
-        """Test method for public instances"""
+        """Test method for public instances
+        """
         r1 = Review()
         r2 = Review(**r1.to_dict())
         self.assertIsInstance(r1.id, str)
@@ -42,20 +46,23 @@ class TestReview(unittest.TestCase):
         self.assertEqual(r1.updated_at, r2.updated_at)
 
     def test_str(self):
-        """Test method for str representation"""
+        """Test method for str representation
+        """
         r1 = Review()
         string = f"[{type(r1).__name__}] ({r1.id}) {r1.__dict__}"
         self.assertEqual(r1.__str__(), string)
 
     def test_save(self):
-        """Test method for save"""
+        """Test method for save
+        """
         r1 = Review()
         old_update = r1.updated_at
         r1.save()
         self.assertNotEqual(r1.updated_at, old_update)
 
     def test_todict(self):
-        """Test method for dict"""
+        """Test method for dict
+        """
         r1 = Review()
         r2 = Review(**r1.to_dict())
         a_dict = r2.to_dict()
