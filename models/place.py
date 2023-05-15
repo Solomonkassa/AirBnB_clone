17a8608ba84d6409de58559b9137da3dd41e10eb
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-15 01:39:02 -0400

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-It defines one class, `Place()`,
+"""Defines one class, `Place()`,
 which sub-classes the `BaseModel()` class.`
 """
 from models.base_model import BaseModel
